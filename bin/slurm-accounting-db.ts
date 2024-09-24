6ae3c8fbdacccf0fe05e6dbd7076488e8fa1e083
Yasutaka OHMURA <ohmura.yasutaka@classmethod.jp>
2024-09-25 07:05:19 +0900

    devio用

--- bin/slurm-accounting-db.ts
+++ bin/slurm-accounting-db.ts
@@ -4,18 +4,14 @@ import * as cdk from 'aws-cdk-lib';
 import { SlurmAccountingDbStack } from '../lib/slurm-accounting-db-stack';
 
 const app = new cdk.App();
+
+const env = {
+  account: '123456789012', // ご自身のAWSアカウントIDに置き換えてください
+  region: 'ap-northeast-1', // デプロイ先のリージョンに置き換えてください
+};
+
 new SlurmAccountingDbStack(app, 'SlurmAccountingDbStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  vpcId: 'vpc-054b8c5b1c1012441', // 既存VPCのIDを入力
+  vpcCidr: '10.0.0.0/16', // VPCのCIDRブロックを入力
+  env,
 });
--- lib/slurm-accounting-db-stack.ts
+++ lib/slurm-accounting-db-stack.ts
@@ -1,23 +1,99 @@
 import * as cdk from 'aws-cdk-lib';
-import { Secret, SecretStringGenerator } from 'aws-cdk-lib/aws-secretsmanager';
-import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
-import { CfnSecret } from 'aws-cdk-lib/aws-secretsmanager';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 
-export class SlurmAccountingDbStack extends Stack {
+export interface SlurmAccountingDbStackProps extends cdk.StackProps {
+  vpcId: string;
+  vpcCidr: string;
+}
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class SlurmAccountingDbStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: SlurmAccountingDbStackProps) {
     super(scope, id, props);
 
-    const slurmDbSecret = new CfnSecret(this, 'SlurmDBPlaintextPassword', {
-      name: 'slurm-db-password',
+    // Secrets Manager でプレーンテキストパスワードを生成
+    const dbPasswordSecret = new secretsmanager.Secret(this, 'SlurmDBPlaintextPassword', {
+      secretName: 'slurmdb-password',
       description: 'Plaintext password for Slurm DB MySQL instance',
       generateSecretString: {
         passwordLength: 30,
-        excludePunctuation: true, // 句読点を除外
-        includeSpace: false,       // スペースを含めない
-        requireEachIncludedType: true, // 各文字種を含む
+        excludePunctuation: true,
+        includeSpace: false,
+        requireEachIncludedType: true,
       },
+    });
+
+    // 既存の VPC を参照
+    const vpc = ec2.Vpc.fromLookup(this, 'ExistingVpc', { vpcId: props.vpcId });
+
+    // サブネットグループの作成
+    const subnetGroup = new rds.SubnetGroup(this, 'SlurmDbSubnetGroup', {
+      description: 'Subnet group for Slurm DB',
+      vpc: vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // セキュリティグループの作成
+    const securityGroup = new ec2.SecurityGroup(this, 'SlurmDbSecurityGroup', {
+      vpc: vpc,
+      description: 'Security group for Slurm DB',
+      allowAllOutbound: true,
+    });
+
+    // VPC CIDR からのインバウンドトラフィックを許可
+    securityGroup.addIngressRule(
+      ec2.Peer.ipv4(props.vpcCidr),
+      ec2.Port.tcp(3306),
+      'Allow MySQL access from VPC'
+    );
+
+    // パラメータグループの作成
+    const parameterGroup = new rds.ParameterGroup(this, 'SlurmDbParameterGroup', {
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_39 }),
+      description: 'Custom parameter group for Slurm DB',
+      parameters: {},
+    });
+
+    // オプショングループの作成
+    const optionGroup = new rds.OptionGroup(this, 'SlurmDbOptionGroup', {
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_39 }),
+      description: 'Custom option group for Slurm DB',
+      configurations: [],
+    });
+
+    // RDS インスタンスの作成
+    const dbInstance = new rds.DatabaseInstance(this, 'SlurmDbInstance', {
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_39 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO), // メモ: t4g.micro は Performance Insights 非対応のタイプ
+      vpc: vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+      subnetGroup: subnetGroup,
+      securityGroups: [securityGroup],
+      allocatedStorage: 20,
+      storageType: rds.StorageType.GP3,
+      storageEncrypted: true,
+      databaseName: 'slurmdb',
+      credentials: rds.Credentials.fromPassword('admin', dbPasswordSecret.secretValue),
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // 注意: 本番環境では RETAIN を検討してください
+      deletionProtection: false, // 注意: 本番環境では true を検討してください
+      parameterGroup: parameterGroup,
+      optionGroup: optionGroup,
+      backupRetention: cdk.Duration.days(7),
+      preferredBackupWindow: '20:00-21:00', // UTC (JST 毎日 5:00-6:00)
+      preferredMaintenanceWindow: 'sun:21:00-sun:22:00', // UTC (JST 日曜日 6:00-7:00)
+    });
+
+    // アウトプット
+    new cdk.CfnOutput(this, 'DatabaseEndpoint', {
+      value: dbInstance.dbInstanceEndpointAddress,
+      description: 'The endpoint of the database',
+    });
+
+    new cdk.CfnOutput(this, 'DatabaseSecretArn', {
+      value: dbPasswordSecret.secretArn,
+      description: 'The arn of the slurmdb password',
     });
   }
 }
